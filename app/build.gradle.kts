1a1673821ee5d08210e8fe6b29138c2ea888d5de
TheMuLing <289313199+TheMuLing@users.noreply.github.com>
2026-08-19 14:34:58 +0800

    chore: 版本号更新为 26.08.19-gamma (26081903)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.luafabric.studio.falling"
         minSdk = 24
         targetSdk = 36
-        versionCode = 26081902
-        versionName = "26.08.19"
+        versionCode = 26081903
+        versionName = "26.08.19-gamma"
 
         vectorDrawables {
             useSupportLibrary = true
