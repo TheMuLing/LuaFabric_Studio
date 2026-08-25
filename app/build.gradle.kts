7fcdf21cb0ff013d06a4940a494d4fbbfaab8ee3
TheMuLing <289313199+TheMuLing@users.noreply.github.com>
2026-08-25 15:22:50 +0800

    build: bump version to 26.08.25 (26082501); remove redundant json lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.luafabric.studio.falling"
         minSdk = 24
         targetSdk = 36
-        versionCode = 26082202
-        versionName = "26.08.22-alpha"
+        versionCode = 26082501
+        versionName = "26.08.25"
 
         vectorDrawables {
             useSupportLibrary = true
